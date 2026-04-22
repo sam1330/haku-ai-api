df3fc3919856442e7587364b71bfd700cfd703c9
Sammy1301 <samuel.martinez@elaniin.com>
2026-04-21 22:34:19 -0400

    implement husky

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    // add custom rules here
+  },
+};
